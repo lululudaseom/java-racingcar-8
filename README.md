d26f96e1d92e3949a96838d81ab1cb458ccb2eae
정다섬 <jeongdaseom@jeongdaseom-ui-MacBookAir.local>
2025-10-27 20:40:30 +0900

    커밋 안된줄 몰랐어요 read.me와 그에 따른 코드 구현 첫번째

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # java-racingcar-precourse
+carName1 = input(“경주할 자동차 이름 입력하세요”)
+-> 콤마를 구분자로 나눈 리스트 carName2 만들기
+
+Number = int(input(“시도할 횟수를 입력하세요”))
+
+Print(“실행결과”)
+
+pos = [“”] * len(carName2)
+
+for(Number만큼):
+for(i = len(carName2)):
+if random.uniform(0,9) >= 4:
+pos[i] = pos[i] + “-“
+print(carName2[I] + “:” + pos[i])
+
+->for, for, if 겹침
+->함수 분리 하자
+
+
+
+Main
+carName1 = input(“경주할 자동차 이름 입력하세요”)
+carName2 = f1(carName1)
+F2(carName2)
+Number = int(input(“시도할 횟수를 입력하세요”))
+Print(“실행결과”)
+
+F1 (splitNames)
+->구분자로 나눈 리스트 carName2 리턴
+
+F2 (nameError)
+->for (len(carName)):
+if (len(name)>5):
+오류출력
+
+F3
+->if random.uniform(0,9) >= 4:
+pos[i] = pos[i] + “-“
+
+F4
+->for(i = len(carName2)):
+f3
+print(carName2[I] + “:” + pos[i])
+
+F5
+->for(Number만큼):
+f4
+
+함수 이름 멀로 정하지?? 나중에 바꾸자 그냥!
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,52 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요");
+        String carName1 = Console.readLine();
+        String[] carName2 = splitNames(carName1);
+        nameError(carName2);
+
+        System.out.println("시도할 횟수를 입력하세요");
+        int number = Integer.parseInt(Console.readLine());
+
+        System.out.println("실행결과");
+        f5(carName2, number);
     }
+    static String[] splitNames(String input) {
+        return input.split(",");
+    }
+    static void nameError(String[] cars) {
+        for (String name : cars) {
+            if (name.trim().length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+    static void f3(String[] cars,  String[] pos, int i) {
+        if (Randoms.pickNumberInRange(0,9) >= 4) {
+            pos[i] = pos [i] + "-";
+        }
+    }
+    static void f4(String[] cars, String[] pos) {
+        for (int i = 0; i < cars.length; i++) {
+            f3(cars, pos, i);
+            System.out.println(cars[i].trim() + " : " + pos[i]);
+        }
+        System.out.println(); //?빼도되는거아니냐
+    }
+    static void f5(String[] cars, int number) {
+        String[] pos = new String[cars.length];
+        Arrays.fill(pos, "");
+
+        for (int r = 0; r < number; r++) {
+            f4(cars, pos);
+        }
+    }
+
 }
